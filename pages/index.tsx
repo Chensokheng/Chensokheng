6c243059607fd52dc3410a423de84e0b86176d72
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2022-04-26 14:32:21 +0700

    fix: hydration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import type { NextPage } from "next";
-import Navbar from "../components/Navbar";
+
 import UserInfo from "../components/UserInfo";
 import Ability from "../components/Ability";
 import ShowCase from "../components/ShowCase";
 import Support from "../components/Support";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-
+import dynamic from "next/dynamic";
+const Navbar = dynamic(() => import("../components/Navbar"), { ssr: false });
 const Home: NextPage = () => {
 	return (
 		<section className="bg-yellow-100 dark:bg-primary min-h-screen">
